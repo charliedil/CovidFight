d76585e9feb8c892c559486eb2a302038a96d28f
hominhtuan <hominhtuan1998@gmail.com>
2020-09-16 16:33:51 -0400

    modify layout for Vcu cases activity

--- app/src/main/java/com/example/covidfight/VcuStat.java
+++ app/src/main/java/com/example/covidfight/VcuStat.java
@@ -8,24 +8,36 @@ import android.widget.TextView;
 
 public class VcuStat extends AppCompatActivity {
 
-    private TextView activeCaseVCUNum;
+    private TextView activeStudentCaseNumber,activeEmployeeCaseNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vcu_stat);
 
-        activeCaseVCUNum=findViewById(R.id.activeCaseNumber);
+        activeStudentCaseNumber=findViewById(R.id.ActiveStudentCaseNumber);
 
-        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 600);
-        valueAnimator.setDuration(5000);
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 124);
+        valueAnimator.setDuration(3000);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                activeCaseVCUNum.setText(valueAnimator.getAnimatedValue().toString());
+                activeStudentCaseNumber.setText(valueAnimator.getAnimatedValue().toString());
             }
         });
         valueAnimator.start();
+
+        activeEmployeeCaseNumber=findViewById(R.id.ActiveEmployeeCaseNumber);
+
+        ValueAnimator valueAnimator2 = ValueAnimator.ofInt(0, 169);
+        valueAnimator2.setDuration(3000);
+        valueAnimator2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                activeEmployeeCaseNumber.setText(valueAnimator.getAnimatedValue().toString());
+            }
+        });
+        valueAnimator2.start();
 
     }
 }

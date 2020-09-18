1ff3bb16033e84651da7ae6d40a9568391729543
hominhtuan <hominhtuan1998@gmail.com>
2020-09-18 00:08:59 -0400

    Add layout element for Vcu statistic activity and methods for animation number

--- app/src/main/java/com/example/covidfight/VcuStat.java
+++ app/src/main/java/com/example/covidfight/VcuStat.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 public class VcuStat extends AppCompatActivity {
 
-    private TextView activeStudentCaseNumber,activeEmployeeCaseNumber;
+    private TextView activeStudentCaseNumber,activeEmployeeCaseNumber,isolationNumber,quarantineNumber,negativeEntryResultNumber,positiveEntryResultNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +38,56 @@ public class VcuStat extends AppCompatActivity {
             }
         });
         valueAnimator2.start();
+
+        isolationNumber=findViewById(R.id.IsolationNumber);
+
+        ValueAnimator valueAnimator3 = ValueAnimator.ofInt(0, 124);
+        valueAnimator3.setDuration(3000);
+        valueAnimator3.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                isolationNumber.setText(valueAnimator.getAnimatedValue().toString());
+            }
+        });
+        valueAnimator3.start();
+
+        quarantineNumber=findViewById(R.id.QuarantineNumber);
+
+        ValueAnimator valueAnimator4 = ValueAnimator.ofInt(0, 124);
+        valueAnimator3.setDuration(3000);
+        valueAnimator3.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                quarantineNumber.setText(valueAnimator.getAnimatedValue().toString());
+            }
+        });
+        valueAnimator4.start();
+
+        negativeEntryResultNumber=findViewById(R.id.NegavtiveEntryResultNumber);
+
+        ValueAnimator valueAnimator5 = ValueAnimator.ofInt(0, 124);
+        valueAnimator5.setDuration(3000);
+        valueAnimator5.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                activeStudentCaseNumber.setText(valueAnimator.getAnimatedValue().toString());
+            }
+        });
+        valueAnimator5.start();
+
+
+        positiveEntryResultNumber=findViewById(R.id.PositiveEntryResultNumber);
+
+        ValueAnimator valueAnimator6 = ValueAnimator.ofInt(0, 124);
+        valueAnimator6.setDuration(3000);
+        valueAnimator6.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                activeStudentCaseNumber.setText(valueAnimator.getAnimatedValue().toString());
+            }
+        });
+        valueAnimator6.start();
+
 
     }
 }

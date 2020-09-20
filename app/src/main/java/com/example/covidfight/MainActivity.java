37765a12acbbe40048dc1f84feed53b7198e9474
Lenice <jacksonlr2@vcu.edu>
2020-09-19 20:42:25 -0400

    Added Home Screen with functional cards to take to selected activity

--- app/src/main/java/com/example/covidfight/MainActivity.java
+++ app/src/main/java/com/example/covidfight/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.covidfight;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public CardView card1, card2, card3, card4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        card1 = (CardView) findViewById(R.id.mapCard);
+        card2 = (CardView) findViewById(R.id.statsCard);
+        card3 = (CardView) findViewById(R.id.businessCard);
+        card4 = (CardView) findViewById(R.id.infoCard);
+
+        card1.setOnClickListener(this);
+        card2.setOnClickListener(this);
+        card3.setOnClickListener(this);
+        card4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch(view.getId()){
+            case R.id.mapCard :
+                i = new Intent(this,MapsActivity.class);
+                startActivity(i);
+                break;
+
+            /*case R.id.statsCard :
+                i = new Intent(this,Statistics.class);
+                startActivity(i);
+                break;
+
+            case R.id.businessCard :
+                i = new Intent(this,BusinessActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.infoCard :
+                i = new Intent(this,);
+                startActivity(i);
+                break;*/
+        }
+
     }
 }
--- app/src/main/java/com/example/covidfight/Splash.java
+++ app/src/main/java/com/example/covidfight/Splash.java
@@ -3,6 +3,8 @@ package com.example.covidfight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+       /* RelativeLayout rl = findViewById(R.id.relativeLayout);
+        ImageView iv = new ImageView(this);
+        iv.setImageResource(R.drawable.logo);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(500,400);
+        params.leftMargin = 100;
+        params.topMargin = 300;
+        rl.addView(iv,params);*/
     }
 }

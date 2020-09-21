69c0dc992ddca605abd85caff13966e77e7f0cfd
hominhtuan <hominhtuan1998@gmail.com>
2020-09-20 23:43:19 -0400

    get Data from VCU, convert JSON object to Java to show data on Statistic page

--- app/src/main/java/com/example/covidfight/VcuStat.java
+++ app/src/main/java/com/example/covidfight/VcuStat.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class VcuStat extends AppCompatActivity {
 
     private TextView activeStudentCaseNumber,activeEmployeeCaseNumber,isolationNumber,quarantineNumber,negativeEntryResultNumber,positiveEntryResultNumber;
+    int studentCase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,20 +34,24 @@ public class VcuStat extends AppCompatActivity {
         setContentView(R.layout.activity_vcu_stat);
 
         activeStudentCaseNumber=findViewById(R.id.ActiveStudentCaseNumber);
+        getData();
 
-        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 124);
-        valueAnimator.setDuration(3000);
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                activeStudentCaseNumber.setText(valueAnimator.getAnimatedValue().toString());
-            }
-        });
-        valueAnimator.start();
+
+
+
+//        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 5);
+//        valueAnimator.setDuration(3000);
+//        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//            @Override
+//            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+//                activeStudentCaseNumber.setText(valueAnimator.getAnimatedValue().toString());
+//            }
+//        });
+//        valueAnimator.start();
 
         activeEmployeeCaseNumber=findViewById(R.id.ActiveEmployeeCaseNumber);
 
-        ValueAnimator valueAnimator2 = ValueAnimator.ofInt(0, 169);
+        ValueAnimator valueAnimator2 = ValueAnimator.ofInt(0, studentCase);
         valueAnimator2.setDuration(3000);
         valueAnimator2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
@@ -54,8 +76,8 @@ public class VcuStat extends AppCompatActivity {
         quarantineNumber=findViewById(R.id.QuarantineNumber);
 
         ValueAnimator valueAnimator4 = ValueAnimator.ofInt(0, 124);
-        valueAnimator3.setDuration(3000);
-        valueAnimator3.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+        valueAnimator4.setDuration(3000);
+        valueAnimator4.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
                 quarantineNumber.setText(valueAnimator.getAnimatedValue().toString());
@@ -70,7 +92,7 @@ public class VcuStat extends AppCompatActivity {
         valueAnimator5.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                activeStudentCaseNumber.setText(valueAnimator.getAnimatedValue().toString());
+                negativeEntryResultNumber.setText(valueAnimator.getAnimatedValue().toString());
             }
         });
         valueAnimator5.start();
@@ -83,11 +105,46 @@ public class VcuStat extends AppCompatActivity {
         valueAnimator6.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                activeStudentCaseNumber.setText(valueAnimator.getAnimatedValue().toString());
+                positiveEntryResultNumber.setText(valueAnimator.getAnimatedValue().toString());
             }
         });
         valueAnimator6.start();
 
 
     }
+
+
+   private  void getData(){
+       final Gson gson= new Gson();
+
+       String url="https://quinn50.dev/vcucovid/api/v1";
+
+
+       StringRequest request=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+           @Override
+           public void onResponse(String response) {
+
+               VcuCase vcuCase=gson.fromJson(response,VcuCase.class);
+               int n=vcuCase.getStudents().size();
+               Data data=vcuCase.getStudents().get(n-1);
+               String x=Integer.toString(data.getValue());
+               activeStudentCaseNumber.setText(x);
+               studentCase=data.getValue();
+           }
+       }, new Response.ErrorListener() {
+           @Override
+           public void onErrorResponse(VolleyError error) {
+               error.printStackTrace();
+           }
+       });
+       RequestQueue queue= Volley.newRequestQueue(this);
+       queue.add(request);
+       queue.start();
+
+
+   }
+
+
+
 }

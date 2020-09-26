6019fbebe5a2819fccd506082860ca84119552ab
hominhtuan <hominhtuan1998@gmail.com>
2020-09-25 23:47:41 -0400

    buid Recycler view for Richmond case page

--- app/src/main/java/com/example/covidfight/RichmondStat.java
+++ app/src/main/java/com/example/covidfight/RichmondStat.java
@@ -1,14 +1,19 @@
 package com.example.covidfight;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class RichmondStat extends AppCompatActivity {
 
+    private RecyclerView richmondCaseRecView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_richmond_stat);
+
+        richmondCaseRecView=findViewById(R.id.RichmondCaseRecyclerView);
     }
 }

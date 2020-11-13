a6852658f7a5de31655bf4b0eb6615039d8c4668
Lenice <jacksonlr2@vcu.edu>
2020-11-13 15:46:37 -0500

    Fixed Settings Bug. Saves their settings text even after exiting activity

--- app/src/main/java/com/example/covidfight/Reminder.java
+++ app/src/main/java/com/example/covidfight/Reminder.java
@@ -12,7 +12,10 @@ import androidx.core.app.NotificationManagerCompat;
 public class Reminder extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationHelper notificationHelper = new NotificationHelper(context);
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
+        notificationHelper.getManager().notify(1, nb.build());
+        /* NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Intent repeating_intent = new Intent(context, MainActivity.class);
         repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(context,100,repeating_intent,PendingIntent.FLAG_UPDATE_CURRENT);
@@ -27,6 +30,6 @@ public class Reminder extends BroadcastReceiver {
 
        // NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
 
-        notificationManager.notify(100, builder.build());
+        notificationManager.notify(100, builder.build());*/
     }
 }

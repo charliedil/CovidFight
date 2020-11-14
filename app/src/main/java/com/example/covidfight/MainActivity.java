bcf4390acb4daee9bb26772157be6c54fab1df6e
Parth <p.kapoorsr@gmail.com>
2020-11-13 21:42:47 -0500

    Google checks for MainAct

--- app/src/main/java/com/example/covidfight/MainActivity.java
+++ app/src/main/java/com/example/covidfight/MainActivity.java
@@ -1,100 +1,94 @@
 package com.example.covidfight;
 
-import android.app.AlarmManager;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
-
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.cardview.widget.CardView;
-
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import java.util.Calendar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public CardView card1, card2, card3, card4, card5, card6, card7;
+  public CardView card1;
+  public CardView card2;
+  public CardView card3;
+  public CardView card4;
+  public CardView card5;
+  public CardView card6;
+  public CardView card7;
 
-    //ensures that the notification pop up only occurs the first the app is launched
-    //adds functionality to each card that is on the home page
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  //ensures that the notification pop up only occurs the first the app is launched
+  //adds functionality to each card that is on the home page
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-        boolean firstStart = prefs.getBoolean("firstStart", true);
-        if(firstStart) {
-            showStartDialog();
-        }
-
-        card1 = (CardView) findViewById(R.id.mapCard);
-        card2 = (CardView) findViewById(R.id.statsCard);
-        card3 = (CardView) findViewById(R.id.businessCard);
-        //card4 = (CardView) findViewById(R.id.infoCard);
-        card5 = (CardView) findViewById(R.id.reportCard);
-        card6 = (CardView) findViewById(R.id.settingsCard);
-        card7 = (CardView) findViewById(R.id.aboutCard);
-
-        card1.setOnClickListener(this);
-        card2.setOnClickListener(this);
-        card3.setOnClickListener(this);
-        //card4.setOnClickListener(this);
-        card5.setOnClickListener(this);
-        card6.setOnClickListener(this);
-        card7.setOnClickListener(this);
+    SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+    boolean firstStart = prefs.getBoolean("firstStart", true);
+    if (firstStart) {
+      showStartDialog();
     }
 
-    //Displays a pop up screen that asks user whether they want to enable notifications
-    private void showStartDialog(){
-        new AlertDialog.Builder(this)
-                .setTitle("Notifications")
-                .setMessage("Would you like like to set a daily reminder to wear your mask?")
-                .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                       Intent start = new Intent(MainActivity.this, SettingsActivity.class);
-                       startActivity(start);
+    card1 = (CardView) findViewById(R.id.mapCard);
+    card2 = (CardView) findViewById(R.id.statsCard);
+    card3 = (CardView) findViewById(R.id.businessCard);
+    //card4 = (CardView) findViewById(R.id.infoCard);
+    card5 = (CardView) findViewById(R.id.reportCard);
+    card6 = (CardView) findViewById(R.id.settingsCard);
+    card7 = (CardView) findViewById(R.id.aboutCard);
 
-                        //createNotificationChannel();
-                        /*Calendar calendar = Calendar.getInstance();
+    card1.setOnClickListener(this);
+    card2.setOnClickListener(this);
+    card3.setOnClickListener(this);
+    //card4.setOnClickListener(this);
+    card5.setOnClickListener(this);
+    card6.setOnClickListener(this);
+    card7.setOnClickListener(this);
+  }
 
-                        calendar.set(Calendar.HOUR_OF_DAY, 8);
-                        calendar.set(Calendar.MINUTE, 0);
-                        calendar.set(Calendar.SECOND, 0);
+  //Displays a pop up screen that asks user whether they want to enable notifications
+  private void showStartDialog() {
+    new AlertDialog.Builder(this)
+        .setTitle("Notifications")
+        .setMessage("Would you like like to set a daily reminder to wear your mask?")
+        .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            Intent start = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(start);
 
-                        Intent intent = new Intent(getApplicationContext(), Reminder.class);
-                        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-                        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);*/
-                    }
-                })
+            //createNotificationChannel();
+            /*Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, 8);
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 0);
+            Intent intent = new Intent(getApplicationContext(), Reminder.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),100,
+            intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
+            calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);*/
+          }
+        })
 
-                .setNegativeButton("Don't Allow", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                })
-                .create().show();
+        .setNegativeButton("Don't Allow", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            dialogInterface.dismiss();
+          }
+        })
+        .create().show();
 
-        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putBoolean("firstStart", false);
-        editor.apply();
-    }
+    SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+    SharedPreferences.Editor editor = prefs.edit();
+    editor.putBoolean("firstStart", false);
+    editor.apply();
+  }
 
-   /* //creates the notification channel that reminds users to wear their mask
+  /* //creates the notification channel that reminds users to wear their mask
     private void createNotificationChannel(){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             CharSequence name = "MaskReminderChannel";
@@ -108,53 +102,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }*/
 
-    //links each card on Home Page to their respective activities
-    @Override
-    public void onClick(View view) {
-        Intent i;
+  //links each card on Home Page to their respective activities
+  @Override
+  public void onClick(View view) {
+    Intent i;
 
-        switch(view.getId()){
-            case R.id.mapCard :
-                i = new Intent(this,MapsActivity.class);
-                startActivity(i);
-                break;
+    switch (view.getId()) {
+      case R.id.mapCard:
+        i = new Intent(this, MapsActivity.class);
+        startActivity(i);
+        break;
 
-            case R.id.statsCard :
-                i = new Intent(this,Statictics.class);
-                startActivity(i);
-                break;
+      case R.id.statsCard:
+        i = new Intent(this, Statictics.class);
+        startActivity(i);
+        break;
 
-            case R.id.businessCard :
-                i = new Intent(this,Business.class);
-                startActivity(i);
-                break;
+      case R.id.businessCard:
+        i = new Intent(this, Business.class);
+        startActivity(i);
+        break;
 
-//            case R.id.infoCard :
-//                i = new Intent(this,);
-//                startActivity(i);
-//                break;
+      //case R.id.infoCard :
+      //i = new Intent(this,);
+      //startActivity(i);
+      // break;
 
-            case R.id.reportCard :
-                i = new Intent(this,ReportActivity.class);
-                startActivity(i);
-                break;
+      case R.id.reportCard:
+        i = new Intent(this, ReportActivity.class);
+        startActivity(i);
+        break;
 
-            case R.id.settingsCard :
-                i = new Intent(this,SettingsActivity.class);
-                startActivity(i);
-                break;
+      case R.id.settingsCard:
+        i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
+        break;
 
-            case R.id.aboutCard :
+      case R.id.aboutCard:
 
-                i = new Intent(this,AboutUsActivity.class);
-                startActivity(i);
-                break;
-
-
-        }
+        i = new Intent(this, AboutUsActivity.class);
+        startActivity(i);
+        break;
 
 
     }
+
+
+  }
 
   /*  private void launchBusiness() {
         Intent intent = new Intent(this, BusinessActivity.class);

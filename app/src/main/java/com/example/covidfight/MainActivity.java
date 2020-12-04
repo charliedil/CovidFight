a2fe81ec251c9fc715e6dc4676b6b0b38c1b573e
Lenice <jacksonlr2@vcu.edu>
2020-12-03 22:30:28 -0500

    Cleaned up unnecessary code

--- app/src/main/java/com/example/covidfight/MainActivity.java
+++ app/src/main/java/com/example/covidfight/MainActivity.java
@@ -1,9 +1,5 @@
 package com.example.covidfight;
 
-import android.app.AlarmManager;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 
@@ -12,14 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.cardview.widget.CardView;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -41,7 +32,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         card1 = (CardView) findViewById(R.id.mapCard);
         card2 = (CardView) findViewById(R.id.statsCard);
         card3 = (CardView) findViewById(R.id.businessCard);
-        //card4 = (CardView) findViewById(R.id.infoCard);
         card5 = (CardView) findViewById(R.id.reportCard);
         card6 = (CardView) findViewById(R.id.settingsCard);
         card7 = (CardView) findViewById(R.id.aboutCard);
@@ -49,7 +39,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         card1.setOnClickListener(this);
         card2.setOnClickListener(this);
         card3.setOnClickListener(this);
-        //card4.setOnClickListener(this);
         card5.setOnClickListener(this);
         card6.setOnClickListener(this);
         card7.setOnClickListener(this);
@@ -65,18 +54,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     public void onClick(DialogInterface dialogInterface, int i) {
                        Intent start = new Intent(MainActivity.this, SettingsActivity.class);
                        startActivity(start);
-
-                        //createNotificationChannel();
-                        /*Calendar calendar = Calendar.getInstance();
-
-                        calendar.set(Calendar.HOUR_OF_DAY, 8);
-                        calendar.set(Calendar.MINUTE, 0);
-                        calendar.set(Calendar.SECOND, 0);
-
-                        Intent intent = new Intent(getApplicationContext(), Reminder.class);
-                        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-                        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);*/
                     }
                 })
 
@@ -93,20 +70,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         editor.putBoolean("firstStart", false);
         editor.apply();
     }
-
-   /* //creates the notification channel that reminds users to wear their mask
-    private void createNotificationChannel(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            CharSequence name = "MaskReminderChannel";
-            String description = "Channel to remind people to wear their masks";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("maskNotify", name, importance);
-            channel.setDescription(description);
-
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }*/
 
     //links each card on Home Page to their respective activities
     @Override
@@ -129,11 +92,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(i);
                 break;
 
-//            case R.id.infoCard :
-//                i = new Intent(this,);
-//                startActivity(i);
-//                break;
-
             case R.id.reportCard :
                 i = new Intent(this,ReportActivity.class);
                 startActivity(i);
@@ -149,17 +107,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 i = new Intent(this,AboutUsActivity.class);
                 startActivity(i);
                 break;
-
-
         }
-
-
     }
-
-  /*  private void launchBusiness() {
-        Intent intent = new Intent(this, BusinessActivity.class);
-        startActivity(intent);
-    }*/
-
-
 }
